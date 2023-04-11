30718f2322cfa173c9cc80442287055955926063
Alessandro Clerici <ale.clericil@gmail.com>
2023-04-11 14:59:01 +0200

    removed ugly TODOs in the code

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-// TODO
-// function for generating text of x words. When a sentence terminates (next = ""), add . and start again (prev = "")
-// only what is convenient to keep in memory should be kept, the rest goes on file/db
-// list of allowed chats
-// logging (with chat codes)
-// cache of sum of probs
-// write to file every x minutes (protection from system crashes)
-// use database instead of json, or at least use a different file per chat
-
 import (
 	"log"
 	"os"
